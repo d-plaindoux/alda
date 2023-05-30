41ab49cc65953ca4f90f1986fbe0e3786cd31fe8
d-plaindoux <d.plaindoux@free.fr>
2023-05-30 09:39:08 +0200

    Prepare Functor/Monad laws checker

--- test/alda/t99_laws.ml
+++ test/alda/t99_laws.ml
@@ -33,6 +33,27 @@ let monad_law1 t v p =
   and r = response @@ rhd s in
   Alcotest.(check (pair (option t) bool)) "return a >> h = h a" l r
 
+let monad_law2 t p =
+  (* m >>= return = m *)
+  let open Control.Monad (Parsec) in
+  let lhd = p >>= return
+  and rhd = p in
+  let s = Parsec.source [] in
+  let l = response @@ lhd s
+  and r = response @@ rhd s in
+  Alcotest.(check (pair (option t) bool)) "m >>= return = m" l r
+
+let monad_law3 t m g h =
+  (* (m >>= g) >>= h = m >>= fun x -> g x >>= h *)
+  let open Control.Monad (Parsec) in
+  let lhd = m >>= g >>= h
+  and rhd = m >>= fun x -> g x >>= h in
+  let s = Parsec.source [] in
+  let l = response @@ lhd s
+  and r = response @@ rhd s in
+  Alcotest.(check (pair (option t) bool))
+    "(m >>= g) >>= h = m >>= fun x -> g x >>= h" l r
+
 (* Quick check should be used here *)
 
 let cases =
@@ -45,4 +66,9 @@ let cases =
           functor_law2 int (( + ) 1) int_of_string @@ return "1" )
     ; test_case "return a >> h = h a" `Quick (fun () ->
           monad_law1 int 1 @@ fun v -> return v )
+    ; test_case "m >> return = m" `Quick (fun () -> monad_law2 int @@ return 1)
+    ; test_case "(m >>= g) >>= h = m >>= fun x -> g x >>= h" `Quick (fun () ->
+          monad_law3 int (return "1")
+            (fun v -> return @@ int_of_string v)
+            return )
     ] )

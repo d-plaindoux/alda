8c524ead55f653298b2307f00df3c00d6b820a59
d-plaindoux <d.plaindoux@free.fr>
2023-06-26 08:31:08 +0200

    Minor refactoring replacing ad-hoc ADT by Either

--- lib/alda/parser/response.ml
+++ lib/alda/parser/response.ml
@@ -1,16 +1,14 @@
-type ('a, 'b) t =
-  | Success of ('a * bool * 'b)
-  | Failure of (string option * bool * 'b)
+type ('a, 'b) t = (string option * bool * 'b, 'a * bool * 'b) Either.t
 
 module Construct = struct
-  let success (a, c, b) = Success (a, c, b)
-  let failure (m, c, b) = Failure (m, c, b)
+  let success (a, c, b) = Either.Right (a, c, b)
+  let failure (m, c, b) = Either.Left (m, c, b)
 end
 
 module Destruct = struct
   let fold ~success ~failure = function
-    | Success (a, c, b) -> success (a, c, b)
-    | Failure (m, c, b) -> failure (m, c, b)
+    | Either.Right (a, c, b) -> success (a, c, b)
+    | Either.Left (m, c, b) -> failure (m, c, b)
 
   let fold_opt =
     let none _ = None in
